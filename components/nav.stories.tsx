a1b9fec370a970c18ca89b9134c490e81b013c4c
Amine <mouhamedaymenhammami@gmail.com>
2020-02-23 01:45:03 +0100

    add storybook viewport and host addons

--- components/nav.stories.tsx
+++ components/nav.stories.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
+import { host } from "storybook-host";
 
 import { Button } from "@storybook/react/demo";
 import Nav from "./nav";
@@ -17,6 +18,23 @@ storiesOf("Button", module)
     <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+storiesOf("storybook-host", module)
+  .addDecorator(
+    host({
+      title: "A host container for components under test.",
+      align: "center middle",
+      background: true,
+      backdrop: true
+    })
+  )
+  .add("default", () => (
+    <Button>
+      <span role="img" aria-label="so cool">
+        hello world
       </span>
     </Button>
   ));
