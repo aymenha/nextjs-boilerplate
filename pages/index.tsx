e375e80f42604556afa1810e5f2b6118fa55fbe8
Amine <mouhamedaymenhammami@gmail.com>
2020-03-26 23:40:23 +0100

    create theme and setup proper ssr cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import WelcomeButton from "../components/WelcomeButton/WelcomeButton";
 
 const useStyles = makeStyles(() => ({
@@ -14,18 +13,15 @@ const useStyles = makeStyles(() => ({
 const Home = () => {
   const classes = useStyles();
   return (
-    <>
-      <CssBaseline />
-      <Box
-        component={Container}
-        className={classes.root}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <WelcomeButton>Welcome</WelcomeButton>
-      </Box>
-    </>
+    <Box
+      component={Container}
+      className={classes.root}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <WelcomeButton>Welcome</WelcomeButton>
+    </Box>
   );
 };
 
---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: indigo["A200"]
+    }
+  }
+});
