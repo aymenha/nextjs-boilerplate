229c5c575d9506ae5df011e714dd805f56cf096c
Amine <mouhamedaymenhammami@gmail.com>
2020-03-29 19:56:11 +0100

    cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,15 @@
 module.exports = {
   stories: ["../components/**/*.stories.tsx"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"]
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]]
+      }
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  }
 };
