4d8f6f51bc6356554174cdfd66c985df5d98fbad
Amine <mouhamedaymenhammami@gmail.com>
2020-05-16 22:36:30 +0100

    remove unused storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
   stories: ["../components/**/*.stories.tsx"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  webpackFinal: async config => {
+  addons: [],
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve("babel-loader"),
       options: {
-        presets: [["react-app", { flow: false, typescript: true }]]
-      }
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
     });
     config.resolve.extensions.push(".ts", ".tsx");
     return config;
-  }
+  },
 };
